6e1a09bfeec5d275a21138829a6776fc7df3ca71
Filip Nilsson <filip.n@outlook.com>
2025-11-19 13:33:48 +0100

    fix: update deploy config
    - Conditional base url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Portfolio-react/'
+export default defineConfig(({ mode }) => {
+  const baseConfig = {
+    plugins: [react()],
+  }
+  
+  if (mode === 'cloudflare') {
+    return {
+      ...baseConfig,
+      base: '/'
+    }
+  }
+  
+  return {
+    ...baseConfig,
+    base: '/portfolio-react/'
+  }
 })
